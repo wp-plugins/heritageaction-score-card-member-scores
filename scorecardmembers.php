ee27e6e801fae181fbbf75e3c579478cd9017f6b
heritageaction <heritageaction@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-11 14:56:00 +0000

    Increment to 1.0.5
    git-svn-id: https://plugins.svn.wordpress.org/heritageaction-score-card-member-scores/trunk@873439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- scorecardmembers.php
+++ scorecardmembers.php
@@ -3,7 +3,7 @@
 Plugin Name: HeritageAction Scorecard Member Score
 Plugin URI: http://wordpress.org/plugins/heritageaction-score-card-member-scores
 Description: HeritageAction Scorecard Members of Congress
-Version: 1.0.4
+Version: 1.0.5
 Author: Heritage Action for America
 Author URI: http://heritageaction.com
 
